be688c477f5be840198204139f9aeaadeb23b6c1
Abhishek Naik <abhisheknaik2k20@gmail.com>
2024-09-30 10:46:44 +0530

    2nd Commit

--- lib/firebase_logic/Login_Auth/login_auth.dart
+++ lib/firebase_logic/Login_Auth/login_auth.dart
@@ -3,7 +3,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:project/Mobile/DashBoard/black_screen.dart';
 
 Future<UserCredential?> loginEmailPass(
     BuildContext context, String email, String password) async {
